a822029b4b20d471e5e9d638e29b233e95a10c14
shimhs1 <shimhs1.hs@gmail.com>
2020-08-05 17:31:06 +0900

    20200805:1722 first commit

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,48 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoldu.book.springboot.service.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            System.out.println("SHS : user is not null="+user.getName()); //SHS 20200726
+        }
+        else {
+            //model.addAttribute("userName", "Hong Gil-Dong");
+            System.out.println("SHS : user is null="); //SHS 20200726
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
+}
